b74474b3abe83222529828ffe90eda92c3f37e36
Yuri Castro do Amaral <yuricda96@gmail.com>
2026-03-29 09:23:07 -0300

    Add ESLint, Prettier, and NVM configuration files; update .gitignore and .prettierrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,8 +6,8 @@ module.exports = {
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
+    'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
     'plugin:prettier/recommended',
   ],
   root: true,
